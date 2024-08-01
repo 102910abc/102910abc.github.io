023da6c026597fa4d9fac9b11829c42ea186c9dd
102910abc <mycow102910@gmail.com>
2024-08-01 14:44:11 -0700

    next

--- script.js
+++ script.js
@@ -23,10 +23,14 @@ function validatePassword(password) {
     return password === "notobviouspassword"
 }
 
+
+
+
+
+
 document.getElementById('keywordInput').addEventListener('submit', function() {
     var keyword = this.value.toLowerCase();
     var response = document.getElementById('response');
-    // var responseText = '';
 
     if (keyword === "basketball") {
         response.textContent = "Basketball is a fast-paced team sport where two teams of five players each compete to score points by shooting a ball through the opponent’s hoop. The game is played on a rectangular court with a hoop at each end. Players must dribble the ball while moving, and they aim to score either 2 or 3 points per field goal, or 1 point per free throw. The game consists of four quarters, with a shot clock limiting the time to take a shot. Key positions include the point guard, shooting guard, small forward, power forward, and center, each with distinct roles in scoring, defending, and playmaking.";
